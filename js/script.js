c199f06840f30604c94dc69cc77d88022e0c2537
Nothing <173809041+nothingfr87@users.noreply.github.com>
2026-06-09 10:46:09 +0300

    Added a new post and added animation for hamburger menu added a light mode

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-const menu = document.querySelector(".ham-menu");
+const body = document.body;
+// Hamburger Menu
+const menu = document.querySelector(".links");
 const list = document.querySelector(".header .links ul");
 
 menu.addEventListener("click", () => {
   list.classList.toggle("open");
+});
+
+// Light/Dark Mode Switcher
+const button = document.querySelector(".switch-mode");
+
+button.addEventListener("click", () => {
+  body.classList.toggle("light");
+  if (body.classList.contains("light")) {
+    button.innerHTML = 'الوضع المظلم  <i class="fa-solid fa-moon"></i>';
+  } else {
+    button.innerHTML = 'الوضع الفاتح  <i class="fa-solid fa-sun"></i>';
+  }
 });
